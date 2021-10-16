d5e744b0ef14d73999f41c3cefb402b1a1a7ff02
Yinuo Hu <huyinuo@live.unc.edu>
2021-10-15 22:51:01 -0400

    resolve result issue

--- admin-site/src/pages/GameManagement/EndGameManage.js
+++ admin-site/src/pages/GameManagement/EndGameManage.js
@@ -13,7 +13,7 @@ const EndGameManage = () =>{
         <>
             <h1 style={{color: "#afe607"}}>Game Result Leaderboard</h1>
             <GameInfos thisGame={thisGame} />
-            <LeaderBoard game = {thisGame}></LeaderBoard>
+            <LeaderBoard key={`board${thisGame.gid.stringValue}`} game = {thisGame}></LeaderBoard>
         </>
     )
 }
@@ -23,11 +23,12 @@ const LeaderBoard = ({game}) =>{
       React.useEffect(() => {
         axios.get(resultsURL).then((response) => {
             //filter the result list for only this game
-          setResultList(response.data.documents.map(item => item.fields).filter(result => result.gid.stringValue==game.gid))
+          setResultList(response.data.documents.map(item => item.fields).filter(result => result.gid.stringValue==game.gid.stringValue))
         });
       }, []);
 
 
+      console.log(resultList)
     let t1List = resultList.filter(result => result.tid.stringValue == "t1").sort((a,b) => b.score.integerValue - a.score.integerValue)
     let t2List = resultList.filter(result => result.tid.stringValue == "t2").sort((a,b) => b.score.integerValue - a.score.integerValue)
     let t1Score = t1List.map(result => parseInt(result.score.integerValue)).reduce((a,b) => a+b,0)
@@ -42,7 +43,7 @@ const LeaderBoard = ({game}) =>{
                     <h3 style={{color:"white"}}>Total Score: {t1Score}</h3>
                     <ListGroup variant="flush">
                         {Array.from(t1List).map((result) => (
-                             <ListGroup.Item>{result.uid.stringValue}: {result.score.integerValue}</ListGroup.Item>
+                             <ListGroup.Item key={`result${result.uid.stringValue}`}>{result.uid.stringValue}: {result.score.integerValue}</ListGroup.Item>
                         ))}
                     </ListGroup>
                 </Col>
@@ -50,7 +51,7 @@ const LeaderBoard = ({game}) =>{
                     <h3 style={{color:"white"}}>Total Score: {t2Score}</h3>
                     <ListGroup variant="flush">
                         {Array.from(t2List).map((result) => (
-                             <ListGroup.Item>{result.uid.stringValue}: {result.score.integerValue}</ListGroup.Item>
+                             <ListGroup.Item key={`result${result.uid.stringValue}`}>{result.uid.stringValue}: {result.score.integerValue}</ListGroup.Item>
                         ))}
                     </ListGroup>
                 </Col>
