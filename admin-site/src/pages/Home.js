9a305333aca6bdfe950a8c757aef242e499dd604
Yinuo Hu <huyinuo@live.unc.edu>
2021-10-16 17:19:53 -0400

    most recent admit-page

--- admin-site/src/pages/Home.js
+++ admin-site/src/pages/Home.js
@@ -26,15 +26,18 @@ const HeaderSection = ()=>{
 }
 
 const GameCards = () =>{
-    const [gameList, setGameList] = React.useState(Array.from([]));
+    const [gameList, setGameList] = React.useState(null);
     React.useEffect(()=>{
         axios.get(gamesURL).then((response) =>{
             setGameList(response.data.documents.map(item => item.fields))
         })
     }, []);
+    if (gameList === null){
+        return <div>Loading...</div>
+    }
     return (
         <>
-            {console.log(gameList)}
+            {console.log(gameList[0].uid.arrayValue.values)}
             {/*grid display for game cards*/}
             <Row xs={1} md={2} className="g-4 gameCards">
               {Array.from(gameList).map((game, idx) => (
