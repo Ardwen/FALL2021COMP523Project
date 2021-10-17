6d0fce3c0fd57681deaa99644387e81bd2948b7a
Yinuo Hu <huyinuo@live.unc.edu>
2021-10-17 12:00:34 -0400

    fixed end game result displacement

--- fanzplay/src/api/api.js
+++ fanzplay/src/api/api.js
@@ -1,4 +1,4 @@
-export const resultsURL = "https://firestore.googleapis.com/v1/projects/fanzplay/databases/(default)/documents/Results?key=AIzaSyComkk4cpC6-8qoh7m0U9PQsAn2f0A15Y0"
+export const resultsURL = "https://us-central1-fanzplay.cloudfunctions.net/api/Results"
 export const questionURL="https://us-central1-fanzplay.cloudfunctions.net/api/Questions"
 export const teamsURL="https://us-central1-fanzplay.cloudfunctions.net/api/Teams";
 // export const gamesURL="https://firestore.googleapis.com/v1/projects/fanzplay/databases/(default)/documents/Games?key=AIzaSyComkk4cpC6-8qoh7m0U9PQsAn2f0A15Y0"
--- fanzplay/src/pages/GameManagement/EndGameManage.js
+++ fanzplay/src/pages/GameManagement/EndGameManage.js
@@ -15,7 +15,7 @@ const EndGameManage = () =>{
                 <div className={"overlay"}>
                     <h1 style={{color: "#afe607"}}>Game Result Leaderboard</h1>
                     <GameInfos thisGame={thisGame} />
-                    <LeaderBoard key={`board${thisGame.gid.stringValue}`} game = {thisGame}></LeaderBoard>
+                    <LeaderBoard key={`board${thisGame.id}`} game = {thisGame}></LeaderBoard>
                 </div>
             </div>
 
@@ -28,16 +28,16 @@ const LeaderBoard = ({game}) =>{
       React.useEffect(() => {
         axios.get(resultsURL).then((response) => {
             //filter the result list for only this game
-          setResultList(response.data.documents.map(item => item.fields).filter(result => result.gid.stringValue==game.gid.stringValue))
+          setResultList(response.data.filter(r => r.gid == game.id))
+
         });
       }, []);
 
-
       console.log(resultList)
-    let t1List = resultList.filter(result => result.tid.stringValue == "t1").sort((a,b) => b.score.integerValue - a.score.integerValue)
-    let t2List = resultList.filter(result => result.tid.stringValue == "t2").sort((a,b) => b.score.integerValue - a.score.integerValue)
-    let t1Score = t1List.map(result => parseInt(result.score.integerValue)).reduce((a,b) => a+b,0)
-    let t2Score = t2List.map(result => parseInt(result.score.integerValue)).reduce((a,b) => a+b,0)
+    let t1List = resultList.filter(result => result.tid == "t1").sort((a,b) => b.score.integerValue - a.score.integerValue)
+    let t2List = resultList.filter(result => result.tid == "t2").sort((a,b) => b.score.integerValue - a.score.integerValue)
+    let t1Score = t1List.map(result => parseInt(result.score)).reduce((a,b) => a+b,0)
+    let t2Score = t2List.map(result => parseInt(result.score)).reduce((a,b) => a+b,0)
     console.log(t1Score)
     console.log(t2Score)
 
@@ -48,7 +48,7 @@ const LeaderBoard = ({game}) =>{
                     <h3 style={{color:"white"}}>Total Score: {t1Score}</h3>
                     <ListGroup variant="flush">
                         {Array.from(t1List).map((result) => (
-                             <ListGroup.Item key={`result${result.uid.stringValue}`}>{result.uid.stringValue}: {result.score.integerValue}</ListGroup.Item>
+                             <ListGroup.Item key={`result${result.uid}`}>{result.uid}: {result.score}</ListGroup.Item>
                         ))}
                     </ListGroup>
                 </Col>
@@ -56,7 +56,7 @@ const LeaderBoard = ({game}) =>{
                     <h3 style={{color:"white"}}>Total Score: {t2Score}</h3>
                     <ListGroup variant="flush">
                         {Array.from(t2List).map((result) => (
-                             <ListGroup.Item key={`result${result.uid.stringValue}`}>{result.uid.stringValue}: {result.score.integerValue}</ListGroup.Item>
+                             <ListGroup.Item key={`result${result.uid}`}>{result.uid}: {result.score}</ListGroup.Item>
                         ))}
                     </ListGroup>
                 </Col>
